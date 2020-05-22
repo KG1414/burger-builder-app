ba53f2ce5757e696428034ca4cca595a266a4c5e
kylegallard <kylegallard@gmail.com>
2020-05-22 20:02:01 +1000

    Now accessing Firebase server axios-orders.js file created to link Firebase. Axios post request in BurgerBuilder.js to send order information to server.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axiosOrders from '../../axios-orders';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -76,7 +76,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue');
+        // alert('You continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            totalPrice: this.state.totalPrice,
+            customer: {
+                name: 'Kyle Gallard',
+                address: {
+                    street: '14 Elf st',
+                    postCode: '3000',
+                    country: 'Australia'
+                },
+                email: 'kylegallard@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axiosOrders.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
 
